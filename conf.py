5d57ebddfc4329795958d557c4bf6c603de82753
thephez <thephez@users.noreply.github.com>
2023-01-17 15:47:06 -0500

    build: ignore README

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 
 # The master toctree document.
 master_doc = 'index'
