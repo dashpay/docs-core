0c7dc7cb487a8464d61a13865fcfcdb8ae9631a2
thephez <thephez@users.noreply.github.com>
2023-01-17 10:26:04 -0500

    build: update config

--- conf.py
+++ conf.py
@@ -32,6 +32,8 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The master toctree document.
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
