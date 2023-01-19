20d702dfc276d39e53e7c1dc8246891d190ecaa1
thephez <thephez@users.noreply.github.com>
2023-01-19 19:22:32 +0000

    feat: start web server for preview after startup

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,7 +1,9 @@
 #/!bin/sh
 
 # Command to run during postCreateCommand
-# Done in script because when done via object the commands are run in parallel (which isn't always desirable)
+# Done in script because when done via object the commands are run in parallel
+#  (which isn't always desirable)
+# https://containers.dev/implementors/spec/#parallel-exec
 # https://containers.dev/implementors/json_reference/#formatting-string-vs-array-properties
 
 pip install -r requirements.txt
