93f37c7db8de1921c6233caa663997b1a91a9baa
thephez <thephez@users.noreply.github.com>
2023-07-12 17:21:32 -0400

    chore: copy logic from pydata-theme to toggle search

--- _static/js/pydata-search-close.js
+++ _static/js/pydata-search-close.js
@@ -1,6 +1,58 @@
+/*******************************************************************************
+ * Search
+ */
+/** Find any search forms on the page and return their input element */
+var findSearchInput = () => {
+  let forms = document.querySelectorAll("form.bd-search");
+  if (!forms.length) {
+    // no search form found
+    return;
+  } else {
+    var form;
+    if (forms.length == 1) {
+      // there is exactly one search form (persistent or hidden)
+      form = forms[0];
+    } else {
+      // must be at least one persistent form, use the first persistent one
+      form = document.querySelector(
+        "div:not(.search-button__search-container) > form.bd-search"
+      );
+    }
+    return form.querySelector("input");
+  }
+};
+
+/**
+ * Activate the search field on the page.
+ * - If there is a search field already visible it will be activated.
+ * - If not, then a search field will pop up.
+ */
+var toggleSearchField = () => {
+  // Find the search input to highlight
+  let input = findSearchInput();
+
+  // if the input field is the hidden one (the one associated with the
+  // search button) then toggle the button state (to show/hide the field)
+  let searchPopupWrapper = document.querySelector(".search-button__wrapper");
+  let hiddenInput = searchPopupWrapper.querySelector("input");
+  if (input === hiddenInput) {
+    searchPopupWrapper.classList.toggle("show");
+  }
+  // when toggling off the search field, remove its focus
+  if (document.activeElement === input) {
+    input.blur();
+  } else {
+    input.focus();
+    input.select();
+    input.scrollIntoView({ block: "center" });
+  }
+};
+
+
 $(document).ready(function(){
-    $(".search__cross").click(function(){
-        $(".search-button__wrapper").hide();
-        // $(".search-button__search-container").remove();
-    });
+  $(".search__cross").click(function(){
+      toggleSearchField();
+      // $(".search-button__wrapper").hide();
+      // $(".search-button__search-container").remove();
+  });
 });
