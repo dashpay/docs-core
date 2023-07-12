f6602fd1ba4cec1e58e0c1f0d5052838dc68a7cc
thephez <thephez@users.noreply.github.com>
2023-07-12 17:16:00 -0400

    chore: try hiding the wrapper instead

--- _static/js/pydata-search-close.js
+++ _static/js/pydata-search-close.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".search__cross").click(function(){
-        $(".search-button__search-container").remove();
+        $(".search-button__wrapper").hide();
+        // $(".search-button__search-container").remove();
     });
 });
