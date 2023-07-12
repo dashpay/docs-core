6b93d4668bbbf480474ab3ca7366f0ec43b81ac9
thephez <thephez@users.noreply.github.com>
2023-07-12 17:05:57 -0400

    fix: remove instead of just hiding

--- _static/js/pydata-search-close.js
+++ _static/js/pydata-search-close.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $(".search__cross").click(function(){
-        $(".search-button__search-container").hide();
+        $(".search-button__search-container").remove();
     });
 });
